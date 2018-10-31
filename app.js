d0a9a9b70095d7694d62cdf8631f827aaedba378
Carter <chalfmann9547@myhta.org>
2018-10-31 13:11:26 -1000

    MVP

--- app.js
+++ app.js
@@ -1,33 +1,71 @@
-const res=document.querySelector("#result");
-const search= document.querySelector("#userinp");
-const btn=document.querySelector("#hi")
+let btn=document.querySelector("#btn");
+let res=document.querySelector("#result");
+let search= document.querySelector("#text");
+
 
 btn.addEventListener("click", function(){
     console.log("hi")
-    axios
-    .get("http://rickandmortyapi.com/api/character/?name=$search")
+    
+    let searchValue = search.value;
+    //.split(' ').join('+');
+console.log('The search value was', searchValue);
+ axios.get(`https://rickandmortyapi.com/api/character/?name=${searchValue}`)
     .then(
     function(response){
     console.log("response", response)
     res.innerHTML=response.data.results.map(results=>{
     return `
-    <div>${results.name}</div>
+    <div id="lolres">
+    <div>Name: ${results.name}</div>
     <br>
-    <div>${results.status} 
+    <div>Status: ${results.status}</div> 
     <br>
-    <img href=${results.image}>
+    <div>Origin:${results.origin.name}</div>
+    <img src=${results.image}>
+    </div>
     `
-    
     })
     .join("")
-    
-    
-    }).catch(function(err){
+     }).catch(function(err){
     console.log("err", err)
     
+    
     });
-
-
-
-
 })
+
+
+
+
+
+// //grabs the variables
+// let btn = document.querySelector('#btn');
+// let result = document.querySelector('#result');
+// let text = document.querySelector('#text');
+ 
+// btn.addEventListener('click' , function(){
+//     let sBar = text.value;
+//     console.log('text.value', text.value);
+//     axios.get(`https://rickandmortyapi.com/api/character/?name=${sBar}`) 
+//     .then(
+//         function(response){
+//          console.log('response', response);
+//          result.innerHTML = response.data.results.map(results =>{
+//              //console.log('results.image', results.image);
+//             return `
+//             <div>name: ${results.name}</div>
+//             <div>status: ${results.status}</div>
+//             <div>origin name: ${results.origin.name}</div>
+//             <div>species: ${results.species}</div>
+//             <br>
+//             <img src = ${results.image}>
+//             `
+//         })
+//         .join('');
+//     })
+//     .catch(
+//         function(rip){
+//         console.log('rip',rip);
+//         });
+// });
+
+
